ab2d0e2c8e2937d64f2d0157d7f9864895d5be52
avishjhalani <avishjhalani20@gmail.com>
2025-12-24 23:01:50 +0530

    new commit

--- connect.js
+++ connect.js
@@ -1,39 +1,8 @@
 const mongoose = require('mongoose');
 
-// Cache the connection for serverless environments
-let cached = global.mongoose;
 
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+async function connectToMongoDB(url){
+    return mongoose.connect(url)
 }
 
-async function connectToMongoDB(url) {
-    // If already connected, return the existing connection
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    // If connection is in progress, wait for it
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        };
-
-        cached.promise = mongoose.connect(url, opts).then((mongoose) => {
-            console.log('MongoDB connected');
-            return mongoose;
-        });
-    }
-
-    try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
-    }
-
-    return cached.conn;
-}
-
-module.exports = { connectToMongoDB };
-
+module.exports = {connectToMongoDB,};
